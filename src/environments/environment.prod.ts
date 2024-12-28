859dd83e6b858d02e28cc54b796af0b312bf231a
VincenzoManto <vincmanto@gmail.com>
2024-12-28 20:14:12 +0100

    feat: add Firebase configuration to production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyArCq3t_shta3FHvxhKyDzhit19h3GAN48',
+    authDomain: 'esperia-cf38d.firebaseapp.com',
+    databaseURL: 'https://esperia-cf38d.firebaseio.com',
+    projectId: 'esperia-cf38d',
+    storageBucket: 'esperia-cf38d.firebasestorage.app',
+    messagingSenderId: '338729314582',
+    appId: '1:338729314582:web:a8b38d6f194540058a0837',
+    measurementId: 'G-R9CPNKPQRE',
+  },
 };
