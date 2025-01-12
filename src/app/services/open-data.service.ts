5c5587056f2c62cb91a3541454a377002b79b7bd
VincenzoManto <vincmanto@gmail.com>
2025-01-12 11:48:09 +0100

    feat: add OpenDataService for fetching data from external API

---
+++ src/app/services/open-data.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OpenDataService {
+
+  private apiUrl = `${environment.api}ps.php`;
+
+  constructor(private http: HttpClient) { }
+
+  getData(): Observable<any> {
+    return this.http.get<any>(this.apiUrl);
+  }
+}
