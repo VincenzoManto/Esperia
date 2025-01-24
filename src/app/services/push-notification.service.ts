9b94661e1c03198c982fd8680c88e57d2f7ebc66
VincenzoManto <vincmanto@gmail.com>
2025-01-24 10:29:02 +0100

    feat: relocate Firebase messaging service to root directory and update service worker registration

--- src/app/services/push-notification.service.ts
+++ src/app/services/push-notification.service.ts
@@ -1,8 +1,7 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Messaging, getToken, onMessage } from '@angular/fire/messaging';
 import { BehaviorSubject } from 'rxjs';
-import { mergeMapTo } from 'rxjs/operators';
 import { environment } from '../../environments/environment';
 
 @Injectable({
@@ -30,16 +29,20 @@ export class PushNotificationService {
         console.log('Permission denied');
     });
 
-    // Get the current FCM token
-    getToken(this._messaging)
-      .then((token) => {
-        console.log('Token', token);
-        this.pToken = token;
-        this._token.next(token);
-        // You can send this token to your server and store it there
-        // You can also use this token to subscribe to topics
-      })
-      .catch((error) => console.log('Token error', error));
+    navigator.serviceWorker.register('/Esperia/firebase-messaging-sw.js')
+    .then((registration) => {
+      // Get the current FCM token
+      getToken(this._messaging, { serviceWorkerRegistration: registration })
+        .then((token) => {
+          console.log('Token', token);
+          this.pToken = token;
+          this._token.next(token);
+          // You can send this token to your server and store it there
+          // You can also use this token to subscribe to topics
+        })
+        .catch((error) => console.log('Token error', error));
+
+    });
 
     // Listen for messages from FCM
     onMessage(this._messaging, {
